e6d008985fae26c0cb7323cffe1e686d5910fa74
Christoph Banton <christophabanton@students.utech.edu.jm>
2023-11-23 11:34:09 -0500

    documentation

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,5 @@
- 
+def cosine_similarity_score(user_answer, correct_answer):
+    vectorizer = TfidfVectorizer() # Initializing TF-IDF Vectorizer
+    tfidf_matrix = vectorizer.fit_transform([user_answer, correct_answer]) # Transforming text to TF-IDF matrix
+    cosine_sim = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2]) # Calculating cosine similarity
+    return cosine_sim[0][0] # Ret
